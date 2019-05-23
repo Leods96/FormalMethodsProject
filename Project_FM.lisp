cd031ddea057edbdd0750814f6921ff08036da1b
leods96 <10529860@polimi.it>
2019-05-23 15:40:35 +0200

    movement of robot and human

--- Project_FM.lisp
+++ Project_FM.lisp
@@ -4,7 +4,7 @@
 ;											;
 ;											;
 ; Matteo Frigerio 928058					;
-; Leonardo Romano ...matricola leo...		;
+; Leonardo Romano 920337             		;
 ;											;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -15,13 +15,13 @@
 
 
 (defvar state-d '(N Y))		;Y is in the cell, N otherwise
-(defvar pos-d  '(1 2 3 4 5 6 7 8 9 10 11))
+(defvar pos-d  '(1 2 3 4 5 6 7 8 9 10 11 12))
 
 (define-array human pos-d state-d)
 (define-array robot pos-d state-d)
 
-
-(defvar movement-human
+;Verify that the human is in one and only one cell in the work's zone
+(defvar onePlaceHuman
 	(alw
 		(&& 
 			(-E- x pos-d (human= x 'Y))
@@ -30,7 +30,9 @@
 					(->
 						(> x y)(!!(&&(human= x 'Y)(human= y 'Y)))))))))
 
-(defvar movement-robot
+
+;Verify that the robot is in one and only one cell in the work's zone
+(defvar onePlaceRobot
 	(alw
 		(&& 
 			(-E- x pos-d (robot= x 'Y))
@@ -40,10 +42,161 @@
 						(> x y)(!!(&&(robot= x 'Y)(robot= y 'Y)))))))))
 
 
+(defvar neverInCellFour
+	(alw
+		(&&
+			(human= 4 'N)
+			(robot= 4 'N))))
+
+(defvar movementHuman
+	(alw
+		(&&	
+			(-> (human= 1 'Y)(||(next(human= 1 'Y)) ;starting from 1
+								(next(human= 2 'Y)) 
+								(next(human= 5 'Y)) 
+								(next(human= 6 'Y)) )) 
+
+			(-> (human= 2 'Y)(||(next(human= 1 'Y)) ;starting from 2
+								(next(human= 2 'Y)) 
+								(next(human= 3 'Y)) 
+								(next(human= 5 'Y)) 
+								(next(human= 6 'Y))
+								(next(human= 7 'Y)))) 
+
+			(-> (human= 3 'Y)(||(next(human= 2 'Y)) ;starting from 3
+								(next(human= 3 'Y)) 
+								(next(human= 6 'Y)) 
+								(next(human= 7 'Y)) 
+								(next(human= 8 'Y))))
+
+			(-> (human= 5 'Y)(||(next(human= 1 'Y)) ;starting from 5
+								(next(human= 2 'Y)) 
+								(next(human= 5 'Y)) 
+								(next(human= 6 'Y)) 
+								(next(human= 9 'Y))
+								(next(human= 10 'Y))))
+
+			(-> (human= 6 'Y)(!! (|| (next(human= 8 'Y)) ;starting from 6
+									 (next(human= 12 'Y)))))
+
+			(-> (human= 7 'Y)(!! (|| (next(human= 1 'Y)) ;starting from 7
+									 (next(human= 5 'Y))
+									 (next(human= 5 'Y)))))		
+
+
+			(-> (human= 8 'Y)(||(next(human= 3 'Y)) ;starting from 8
+								(next(human= 7 'Y)) 
+								(next(human= 8 'Y)) 
+								(next(human= 11 'Y)) 
+								(next(human= 12 'Y))))
+
+
+			(-> (human= 9 'Y)(||(next(human= 5 'Y)) ;starting from 9
+								(next(human= 6 'Y)) 
+								(next(human= 9 'Y)) 
+								(next(human= 10 'Y))))
+
+
+			(-> (human= 10 'Y)(||(next(human= 5 'Y)) ;starting from 10
+								(next(human= 6 'Y)) 
+								(next(human= 7 'Y)) 
+								(next(human= 9 'Y)) 
+								(next(human= 10 'Y))
+								(next(human= 11 'Y))))
+
+			
+			(-> (human= 11 'Y)(||(next(human= 6 'Y)) ;starting from 11
+								(next(human= 7 'Y)) 
+								(next(human= 8 'Y)) 
+								(next(human= 10 'Y)) 
+								(next(human= 11 'Y))
+								(next(human= 12 'Y))))
+
+			(-> (human= 12 'Y)(||(next(human= 7 'Y)) ;starting from 12
+								(next(human= 8 'Y)) 
+								(next(human= 11 'Y)) 
+								(next(human= 12 'Y))))
+
+			)))
+
+
+(defvar movementRobot
+	(alw
+		(&&	
+			(-> (robot= 1 'Y)(||(next(robot= 1 'Y)) ;starting from 1
+								(next(robot= 2 'Y)) 
+								(next(robot= 5 'Y)) 
+								(next(robot= 6 'Y)) )) 
+
+			(-> (robot= 2 'Y)(||(next(robot= 1 'Y)) ;starting from 2
+								(next(robot= 2 'Y)) 
+								(next(robot= 3 'Y)) 
+								(next(robot= 5 'Y)) 
+								(next(robot= 6 'Y))
+								(next(robot= 7 'Y)))) 
+
+			(-> (robot= 3 'Y)(||(next(robot= 2 'Y)) ;starting from 3
+								(next(robot= 3 'Y)) 
+								(next(robot= 6 'Y)) 
+								(next(robot= 7 'Y)) 
+								(next(robot= 8 'Y))))
+
+			(-> (robot= 5 'Y)(||(next(robot= 1 'Y)) ;starting from 5
+								(next(robot= 2 'Y)) 
+								(next(robot= 5 'Y)) 
+								(next(robot= 6 'Y)) 
+								(next(robot= 9 'Y))
+								(next(robot= 10 'Y))))
+
+			(-> (robot= 6 'Y)(!! (|| (next(robot= 8 'Y)) ;starting from 6
+									 (next(robot= 12 'Y)))))
+
+			(-> (robot= 7 'Y)(!! (|| (next(robot= 1 'Y)) ;starting from 7
+									 (next(robot= 5 'Y))
+									 (next(robot= 5 'Y)))))		
+
+
+			(-> (robot= 8 'Y)(||(next(robot= 3 'Y)) ;starting from 8
+								(next(robot= 7 'Y)) 
+								(next(robot= 8 'Y)) 
+								(next(robot= 11 'Y)) 
+								(next(robot= 12 'Y))))
+
+
+			(-> (robot= 9 'Y)(||(next(robot= 5 'Y)) ;starting from 9
+								(next(robot= 6 'Y)) 
+								(next(robot= 9 'Y)) 
+								(next(robot= 10 'Y))))
+
+
+			(-> (robot= 10 'Y)(||(next(robot= 5 'Y)) ;starting from 10
+								(next(robot= 6 'Y)) 
+								(next(robot= 7 'Y)) 
+								(next(robot= 9 'Y)) 
+								(next(robot= 10 'Y))
+								(next(robot= 11 'Y))))
+
+			
+			(-> (robot= 11 'Y)(||(next(robot= 6 'Y)) ;starting from 11
+								(next(robot= 7 'Y)) 
+								(next(robot= 8 'Y)) 
+								(next(robot= 10 'Y)) 
+								(next(robot= 11 'Y))
+								(next(robot= 12 'Y))))
+
+			(-> (robot= 12 'Y)(||(next(robot= 7 'Y)) ;starting from 12
+								(next(robot= 8 'Y)) 
+								(next(robot= 11 'Y)) 
+								(next(robot= 12 'Y))))
+
+			)))
+
 
 (eezot:zot 20
 	(&&
-		movement-human
-		movement-robot
+		onePlaceRobot
+		onePlaceHuman
+		neverInCellFour
+		movementHuman
 		)
 	)
